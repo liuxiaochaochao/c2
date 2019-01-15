e6a8ec4f4ab0572090eccc7c99d9b7a6fc2d46d2
chao <chao@qq.com>
2019-01-15 19:25:45 +0800

    第二次

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-char arr[1024]; 
-int index = 0; 
+char arr[1024];
+int index = 0;
 int main()
 {
     printf("敏感字符管理系统\n");
@@ -16,16 +16,41 @@ int main()
         scanf("%d", &code);
         if (code == 1)
         {
-            
+            printf("请输入要添加的敏感字符\n");
+            char m;
+            scanf("%c", &m);
+            scanf("%c", &m);
+            arr[index] = m;
+            index++;
+
+            printf("添加成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
 
         if (code == 2)
         {
-           
+            printf("删除最后一个敏感字符\n");
+            index--;
+            printf("删除成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 3)
         {
-          
+             printf("所有敏感字符如下：\n");
+
+            for (int i = 0; i < index; i++)
+            {
+                printf("第%d个敏感字符为%c\n", i + 1, arr[i]);
+            }
+
+            printf("删除成功，点击回车继续\n");
+            char x;
+            scanf("%c", &x);
+            scanf("%c", &x);
         }
         if (code == 4)
         {
